cc418fc9cb60a019877d48d4c094288fec705d3c
Muhammad Hasin Shabbir <mhs581@nyu.edu>
2022-12-10 22:38:46 +0400

    Update helper.c

--- helper.c
+++ helper.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//helper to get file size
+
 int get_file_size(char* fname) {
 	FILE *fptr;
     fptr = fopen(fname,"r");
     if (fptr == NULL) {
-        perror("File Length Failed.");
         return -1;
     }
 	fseek(fptr,0,SEEK_END);
